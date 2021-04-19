fd7faad543b74fc3fe965a3ca8c84aa96dab49bc
Ryan Hoffman <ryan@architectryan.com>
2021-04-19 09:42:25 -0400

    Hardcode index name

--- src/@rocketseat/gatsby-theme-docs/components/Sidebar/index.js
+++ src/@rocketseat/gatsby-theme-docs/components/Sidebar/index.js
@@ -50,7 +50,7 @@ export default function Sidebar({ isMenuOpen }) {
     );
   }
 
-  const searchIndices = [{ name: process.env.ALGOLIA_INDEX, title: `Pages` }];
+  const searchIndices = [{ name: `dev-esphome-devices`, title: `Pages` }];
 
   return (
     <Container isMenuOpen={isMenuOpen}>
