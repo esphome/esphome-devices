d5a05a0fab62d15d5777e14e422177ad80a02626
Ryan Hoffman <ryan@architectryan.com>
2021-04-21 09:47:17 -0400

    continueOnFailure for gatsby-plugin-algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_ADMIN_KEY,
         queries: require("./src/utils/algolia-queries"),
+        continueOnFailure: true,
       },
     },
   ],
